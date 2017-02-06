fc838a06e070786ce8ffc4c9663e778444647e67
lennseg <lennseg@gmail.com>
2017-02-06 10:09:37 -0500

    LED blinking code

---
+++ week2/led-test/led-test.ino
@@ -0,0 +1,30 @@
+int yellow=13;
+int green=9;
+int blue=4;
+
+
+void setup() { 
+  // the setup routine runs once when you press reset:              
+  // initialize the digital pin as an output.
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT); 
+}
+
+  // put your main code here, to run repeatedly:
+  // the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(yellow, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(250);                   // wait for a second
+  digitalWrite(yellow, LOW);    // turn the LED off by making the voltage LOW
+  delay(250); 
+  {digitalWrite(green, HIGH);
+  delay(250);
+  digitalWrite(green, LOW);
+  delay(250);}
+  {digitalWrite(blue, HIGH);
+  delay(250);
+  digitalWrite(blue, LOW);
+  delay(250);}// wait for a second
+}
+
